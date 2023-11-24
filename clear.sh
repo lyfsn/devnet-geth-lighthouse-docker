5f1933751887cb7dd910b8411524b6465df5ceae
wangyufsn <dev.wangyu@proton.me>
2023-11-24 12:04:46 +0800

    up

---
+++ clear.sh
@@ -0,0 +1,6 @@
+rm -rvf execution-data
+rm -rvf consensus-data
+
+rm -rvf validator-keys/node-1-keystores/keys/logs
+rm -rvf validator-keys/node-1-keystores/keys/slashing_protection.sqlite
+rm -rvf validator-keys/node-1-keystores/keys/slashing_protection.sqlite-journal
--- down.sh
+++ down.sh
@@ -2,9 +2,4 @@
 
 docker compose -f compose.yaml down
 
-rm -rvf execution-data
-rm -rvf consensus-data
 
-rm -rvf validator-keys/node-0-keystores/keys/logs
-rm -rvf validator-keys/node-0-keystores/keys/slashing_protection.sqlite
-rm -rvf validator-keys/node-0-keystores/keys/slashing_protection.sqlite-journal
