54dc98181b52538384b88216a6992a1f9b9368c4
wangyufsn <dev.wangyu@proton.me>
2023-11-24 13:29:57 +0800

    push

--- clear.sh
+++ clear.sh
@@ -1,6 +1,6 @@
-rm -rvf execution-data
-rm -rvf consensus-data
-
-rm -rvf validator-keys/node-1-keystores/keys/logs
-rm -rvf validator-keys/node-1-keystores/keys/slashing_protection.sqlite
-rm -rvf validator-keys/node-1-keystores/keys/slashing_protection.sqlite-journal
+sudo rm -rvf execution-data
+sudo rm -rvf consensus-data
+sudo 
+sudo rm -rvf validator-keys/node-1-keystores/keys/logs
+sudo rm -rvf validator-keys/node-1-keystores/keys/slashing_protection.sqlite
+sudo rm -rvf validator-keys/node-1-keystores/keys/slashing_protection.sqlite-journal
